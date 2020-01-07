27f30212afd17286d91b194c48a77b7eae7d5211
Dj <ritchey0713@gmail.com>
2020-01-07 15:10:48 -0500

    build auth call

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,12 +2,20 @@ import React, { Component } from "react"
 import ClientObj from "./APIs/ApiSecrets"
 
 class GoogleAuth extends Component {
+  state = {
+    isSignedIn: null
+  }
 
   componentDidMount(){
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId: ClientObj.clientId,
         scope: 'email'
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance()
+        this.setState({
+          isSignedIn: this.auth.isSignedIn.get()
+        })
       })
     })
   }
