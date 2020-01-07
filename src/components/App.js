28fd5869af8fae0a09ffa9b80357e15159568f1d
Dj <ritchey0713@gmail.com>
2020-01-07 13:27:34 -0500

    add boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { BrowserRouter, Route, Link } from "react-router-dom"
+
+
+
+const App = () => {
+  return (
+    <div>
+      APP
+    </div>
+  )
+}
+
+export default App
