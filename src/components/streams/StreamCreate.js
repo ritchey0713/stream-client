43ef184a604c6b9ded4ede527c65b34c6e562b2e
Dj <ritchey0713@gmail.com>
2020-01-14 17:43:10 -0500

    refactor stream create

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,39 +1,10 @@
 import React, { Component } from "react"
-import { Field, reduxForm } from 'redux-form'
 import { connect } from 'react-redux'
 import { createStream } from "../../actions/index"
+import StreamForm from "./StreamForm"
 
 
 class StreamCreate extends Component{
-  
-  renderError = ({ error, touched }) => {
-    if(error && touched){
-      return (
-        <div className="ui error message">
-          <div className="header">
-            {error}
-          </div>
-        </div>
-      )
-    }
-  }
-
-  renderInput = ({ input, label, meta }) => {
-    const classError = `field ${meta.error && meta.touched ? "error" : ""}`
-    
-    return(
-      <div className={classError}>
-        <label>{label}</label>
-        <input 
-          {...input }
-          autoComplete="off"
-        />
-        {this.renderError(meta)}
-      </div>
-      
-    )
-  }
-
   onSubmit = (formValues) => {
     this.props.createStream(formValues)
   }
@@ -41,36 +12,16 @@ class StreamCreate extends Component{
   render(){
     return (
       <div>
-        <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
-            <Field name="title" component={this.renderInput} label="Enter Title"/>
-            <Field name="description" component={this.renderInput} label="Enter Description" />
-  
-          <button className="ui button primary">Submit</button>
-        </form>
+        <h3>
+          Create a stream
+        </h3>
+        <StreamForm  onSubmit={this.onSubmit}/>
       </div>
     )
 
   }
 }
 
-const validate = (formValues) => {
-  const errors = {}
-  if(!formValues.title){
-    errors.title = "Must provide a title"
-  }
-
-  if (!formValues.description ){
-    errors.description = "Must provide a description"
-  }
-
-  return errors
-}
-
-const formWrapped = reduxForm({
-  form: 'streamCreate',
-  validate
-})(StreamCreate)
-
 export default connect(null, {
   createStream
-})(formWrapped)
+})(StreamCreate)
