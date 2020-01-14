036ed25defc21471c3f74c36a630e41735150ec3
Dj <ritchey0713@gmail.com>
2020-01-14 18:16:05 -0500

    remove lodash

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -1,7 +1,5 @@
 import { CREATE_STREAM, FETCH_STREAM, FETCH_STREAMS, EDIT_STREAM, DELETE_STREAM } from "../actions/types"
 
-import _ from "lodash"
-
 export default (state = {}, action) => {
   switch(action.type){
     case CREATE_STREAM:
@@ -10,10 +8,19 @@ export default (state = {}, action) => {
         [action.payload.id]: action.payload
       }
 
+    // case FETCH_STREAMS:
+    //   return {
+    //     ...state,
+    //     ..._.mapKeys(action.payload, "id")
+    //   }
+
     case FETCH_STREAMS:
       return {
         ...state,
-        ..._.mapKeys(action.payload, "id")
+        ...action.payload.reduce((newObj, eleObj) => {
+          newObj[eleObj.id] = eleObj
+          return newObj
+        }, {})
       }
 
     case FETCH_STREAM: 
@@ -29,7 +36,9 @@ export default (state = {}, action) => {
       } 
       
     case DELETE_STREAM:
-      return _.omit(state, action.payload)
+      return state.filter(({ id }) => {
+        return id !== action.payload
+      })
 
     default:
       return state
