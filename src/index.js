f6a08944136cee1de120bdc88b47e4e533219412
Dj <ritchey0713@gmail.com>
2020-01-16 16:02:33 -0500

    fix namespace

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware, compose } from "redux"
 import reduxThunk from 'redux-thunk'
 
 import App from "./components/App"
-import reducers from "./reducers"
+import reducers from "./reducers/index.js"
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose 
 
