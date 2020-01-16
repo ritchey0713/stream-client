53fff0a224300ec4926d0e4fd9d035325042ed94
Dj <ritchey0713@gmail.com>
2020-01-16 16:02:13 -0500

    add styles

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-const Modal = () => {
-  return(
+const Modal = (props) => {
+  return ReactDOM.createPortal(
+    <div className="ui dimmer modals visible active">
+      <div className="ui standard modal visible active">
+        <div className="header"> Delete Stream </div>
+        <div className="content">
+          <div className="actions">
+            <button class="ui red button"> Delete</button>
+            <button class="ui button"> Cancel</button>
+          </div>
+        </div>
+      </div> 
+    </div>,
 
+    document.querySelector('#modal')
   )
 }
 
