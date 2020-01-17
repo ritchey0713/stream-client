c12204e67da5e56b26be0d9edf9b72f74c4e40cc
Dj <ritchey0713@gmail.com>
2020-01-17 12:24:16 -0500

    fix html structure

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -2,10 +2,21 @@ import React from "react"
 import Modal from "../Modal"
 
 const StreamDelete = () => {
+  const actions = (
+    <React.Fragment>
+      <button className="ui red button"> Delete</button>
+      <button className="ui button"> Cancel</button>
+    </React.Fragment>
+  )
+
   return (
     <div>
       StreamDelete
-      <Modal />
+      <Modal
+        title="Delete stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+       />
     </div>
   )
 }
